ba53702eb8b2292f06b9c440e83e0329a2c87340
AlokPandey123 <128793825+AlokPandey123@users.noreply.github.com>
2024-06-03 00:56:49 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ScrollTop from "./ScrollTop";
+import linkedin from "../assets/linkedin.png";
+import instagram from "../assets/instagram.png"
+
 
 const Footer = () => {
     return (
-        <footer className="footer p-5 bg-base-100 text-black flex justify-between mt-10 text-black">
+        <footer className="footer p-5 bg-blue-300 text-black flex justify-between mt-10 text-black">
             <nav className="p-4">
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <h6 className="footer-title">Social Media</h6>
+
+                <div className="flex justify-center items-center">
+                    <img src={linkedin} alt="" className="w-10 p-2"/>
+                    <a href="https://www.linkedin.com/company/102731568/admin/settings/" className="link link-hover" target="_blank">Linkedin</a>
+                </div>
+                <div className="flex justify-center items-center">
+                    <img src={instagram} alt="" className="w-10 p-2"/>
+                    <a href="https://www.instagram.com/red__zet/" className="link link-hover" target="_blank">Instagram</a> </div>
+               
             </nav>
             <nav className="p-4">
                 <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <ScrollTop />
+                <Link className="link link-hover" to={`./about`}>About</Link>
+                <Link className="link link-hover" to={`./blog`}>Blogs</Link>
+                <Link className="link link-hover" to={`./contact`}>Contact</Link>
+                <a href="https://forms.gle/ZAjSXdWvF3QMd5Dr9" className="link link-hover" target="_black">Jobs</a>
             </nav >
             <nav className="p-4">
                 <h6 className="footer-title">Legal</h6>
